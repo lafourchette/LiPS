6e1eae334d7ca71bc9453392fe3f36f72b854791
atondelier <atondelier@lafourchette.com>
2014-02-19 10:40:31 +0100

    add documentation, LipsImplementation#on now returns the unregistering function

--- implementor.js
+++ implementor.js
@@ -3,28 +3,72 @@
 var util = require('util');
 
 
+/**
+ *
+ * @param {object} [options]
+ * @param {Function} [options.parser]
+ * @constructor
+ */
 function LipsImplementation(options) {
     if(options) {
         if(options.parser) {
             this.parser = options.parser;
         }
     }
+
+    this._events = {};
 }
 
+LipsImplementation._increment = 0;
+
+/**
+ *
+ * @param {*} content
+ * @returns {*}
+ */
 LipsImplementation.prototype.parser = function(content) {
     return content;
 };
 
+/**
+ *
+ * @param {object} eventOptions
+ * @param {Function} callback
+ * @returns {Function} disable the listener
+ */
 LipsImplementation.prototype.on = function(eventOptions, callback) {
     this.listen(eventOptions, this.parser, callback);
+
+    var currentIncrement = LipsImplementation._increment;
+
+    this._events[currentIncrement] = {
+        eventOptions: eventOptions,
+        callback: callback
+    };
+    LipsImplementation._increment++;
+
+    var self = this;
+
+    return function off() {
+        delete self._events[currentIncrement];
+    };
 };
 
-
+/**
+ *
+ * @param {Function} listen
+ * @param {Function} [implementationParser]
+ * @returns {SpecificLipsImplementation}
+ */
 function createImplementation(listen ,implementationParser) {
 
     //TODO: assertions
     assert.ok(typeof listen === "function", 'listenerInstallation should be a function');
 
+    /**
+     * @inheritDoc LipsImplementation
+     * @constructor
+     */
     function SpecificLipsImplementation() {
         LipsImplementation.apply(this, arguments);
     }
@@ -36,7 +80,6 @@ function createImplementation(listen ,implementationParser) {
 
 
     return SpecificLipsImplementation;
-
 }
 
 exports.createImplementation = createImplementation;
