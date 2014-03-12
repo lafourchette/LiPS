db1a940cb4080286d41a937640c81b29f4960f4a
atondelier <atondelier@lafourchette.com>
2014-03-12 15:21:22 +0100

    add ON_DEATH module to handle process death

--- demo.js
+++ demo.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var path = require('path');
+var ON_DEATH = require('death');
 
 var LiPS = require('./index.js');
 
@@ -8,10 +9,19 @@ var fileLiPS = LiPS.create({
     implementation: 'file-lips'
 });
 
-fileLiPS.on({
-    filepath: path.join(__dirname, 'test.txt'),
+var listener = fileLiPS.on({
+    filepath: path.join(__dirname, 'plop'),
     fileEncoding: 'utf8',
     readingMethod: 'append'
 }, function(err, data) {
     if(!err) console.log('content [', data, ']');
+});
+
+ON_DEATH(function() {
+    console.log('terminating listeners');
+    listener();
+    setTimeout(function() {
+        console.log('exiting process');
+        process.exit();
+    } ,1e3);
 });
